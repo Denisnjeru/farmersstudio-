7cba36b02c5b64055bd10fa0861619c16c7e007b
Denisnjeru <njerudenis0@gmail.com>
2018-11-17 16:55:03 +0300

    :sparkles:

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,15 @@
 from  django.urls import path , include
 from economic import settings
-from .views import *
+from . import views
 from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('my_account/', views.myaccount, name='my_account'),
-    path('order_details/', views.orderdetails, name='order_details'),
-    path('order_info/', views.orderinfo, name='order_info'),
+    path('my_account/', views.my_account, name='my_account'),
+   # path('order_details/', views.orderdetails, name='order_details'),
+   # path('order_info/', views.orderinfo, name='order_info'),
     path('login/', auth_views.auth_login(), name='login'),
 ]
 
